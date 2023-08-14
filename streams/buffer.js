6064b0d63585bfcb5f5ba610492a03f1bf0bdf14
Matheus <matheusdeamorim20@gmail.com>
2023-08-14 10:22:25 -0300

    initial commit

---
+++ streams/buffer.js
@@ -0,0 +1,7 @@
+// buffer é uma representação de um espaço na memória do computador 
+// usado especialmente para transacionar informações rapidamente
+// métodos para ler e salvar informações de forma performática 
+
+const buf = Buffer.from("ok")
+
+console.log(buf)
