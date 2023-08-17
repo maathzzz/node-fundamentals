669e59209ba4cb2513e033dd9945e31279fc7ac2
Matheus <matheusdeamorim20@gmail.com>
2023-08-17 14:43:02 -0300

    remove streams folder

--- streams/buffer.js
+++
@@ -1,7 +0,0 @@
-// buffer é uma representação de um espaço na memória do computador 
-// usado especialmente para transacionar informações rapidamente
-// métodos para ler e salvar informações de forma performática 
-
-const buf = Buffer.from("ok")
-
-console.log(buf)
